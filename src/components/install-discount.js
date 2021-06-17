4dc3b322eb9b2227d1eea9114312ee9f20fa278b
twilightscapes <ihatespam@gmail.com>
2021-06-17 14:04:05 -0500

    Update install-discount.js

--- src/components/install-discount.js
+++ src/components/install-discount.js
@@ -76,7 +76,7 @@ left:'0', right:'0', display:'flex', justifyContent:'center', alignItems:'center
 	<p style={{textAlign:'center', fontSize:'23px', padding:'0', margin:'0 0 10px 0'}}>Install Twilightscapes - <strong>Get 30% off</strong></p>
 	
 	<p style={{fontSize:'100%', textAlign:'left',}}>
-  This website has app functionality. Add it to your home screen to get added features on-the-go, like exclusive content, fullscreen and offline capability. </p>
+  Twilightscapes has app functionality. Add it to your home screen to get added features, exclusive content, and fullscreen capability. </p>
     
     <p style={{fontSize:'100%', }}>You'll receive a special coupon code for 30% off your next order!</p>
 
