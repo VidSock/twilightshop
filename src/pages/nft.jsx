5387123881b7299b882e425c2698a1c3649be1fc
twilightscapes <ihatespam@gmail.com>
2021-06-25 08:58:18 -0500

    Update nft.jsx

--- src/pages/nft.jsx
+++ src/pages/nft.jsx
@@ -148,6 +148,7 @@ export const query = graphql`
     
     
 <Seo title={`NFT Collectible Photography Nightscape Photos`} />
+<Seo description={`We have the NFT you're looking for! Check it out!`} />
 
 
 {/* <div className="video-background">
