ee4b7dfcb2f590019a9fb60016a0cb7db4b12b1e
twilightscapes <ihatespam@gmail.com>
2021-06-25 10:46:25 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,12 @@ require("dotenv").config()
 module.exports = {
   siteMetadata: {
     siteTitle: "Twilightscapes - Unique night photography of abandoned urban exploration",
-    siteTitleDefault: "Twilightscapes - Unique night photography",
+    siteTitleDefault: "Twilightscapes - Unique night photography of abandoned urban exploration",
     siteUrl: "https://twilightscapes.com",
     hrefLang: "en",
     siteDescription:
       "Night long exposure photography in remote abandoned locations urban exploration",
-    siteImage: "https://twilightscapes.com/default-og-image.jpg",
+    siteImage: "/default-og-image.jpg",
     twitter: "toddlambert",
   },
   flags: {
